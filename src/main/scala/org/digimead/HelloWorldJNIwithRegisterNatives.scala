b928363477c278c5c50eb94777b3dec4997cb1ab
matthew <knavely@gmail.com>
2013-09-09 22:39:16 -0700

    scala calling a dictionary implemented in C

--- src/main/scala/org/digimead/HelloWorldJNIwithRegisterNatives.scala
+++ src/main/scala/org/digimead/HelloWorldJNIwithRegisterNatives.scala
@@ -8,15 +8,28 @@ object HelloWorldJNIwithRegisterNatives {
   else {
     println("java.library.path: " + System.getProperty("java.library.path"))
     System.loadLibrary("HelloWorldJNIwithRegisterNatives")
+    
   }
 
   @native
   def add(a: Int, b: Int): Int
   @native
   def hello(): String
+  
+  @native
+  def badd(a: Int, b: Int): Int
+ 
+  @native 
+  def load_dictionary()
+  
+  @native 
+  def free_dictionary()
+  
 
   def main(args: Array[String]) {
     println(hello + ", 5 + 5 = " + add(5,5))
+    load_dictionary()
+    free_dictionary()
   }
 }
 
